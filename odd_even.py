7e6db2dc1ac8bedbed9adcf3b6aa98f75df79754
deepalipopli57 <deepalipopli57@gmail.com>
2018-01-12 00:18:35 +0530

    2 prog added for str and no

---
+++ odd_even.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a no"))
+mod = num % 2
+if mod > 0:
+    print("Odd no")
+else:
+    print("even no")
--- string.py
+++ string.py
@@ -20,3 +20,17 @@ def repeat(str,n):
 print(repeat("Hi",2))
 print(repeat("Deepi",4))
 
+
+#Write a Python program to get the n (non-negative integer) copies of the first 2 characters of a given string.
+# Return the n copies of the whole string if the length is less than 2.
+from 23 https://www.w3resource.com/python-exercises/python-basic-exercise-23.php
+
+
+
+
+
+
+
+
+
+
